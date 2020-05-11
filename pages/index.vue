e63d617fcc5e0529cb11afc05d7b0b4f9da777e4
qsaad <alpeshk_98@yahoo.com>
2020-05-11 06:02:05 -0400

    Initial Commit

---
+++ pages/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <v-row>
+      <v-col>
+        {{message}}
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script>
+//import Logo from '~/components/Logo.vue'
+//import VuetifyLogo from '~/components/VuetifyLogo.vue'
+
+
+export default {
+  components: {
+  
+  },//COMPONENTS
+  data () {
+    return {
+      message : 'HELLO WORLD'
+    }
+  },
+  computed:{
+    myText(){
+      return this.message
+    
+    }
+  }
+  }//EXPORT
+</script>
