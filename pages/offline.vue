5c6687d8f4ec578a1603eccc9098fb72f03b990d
qsaad <alpeshk_98@yahoo.com>
2020-05-14 05:21:18 -0400

    Added Manifest and Service Worker

--- pages/offline.vue
+++ pages/offline.vue
@@ -1,14 +1,13 @@
 <template>
   <div>
-    <app-bar-component path="login" name="Login"></app-bar-component>
+    <app-bar-component path="/" name="Home"></app-bar-component>
     <v-row justify="center" align="center" class="section-one">
       <v-col cols="12" sm="8" md="6" justify="center" align="center" >
         <!-- <img src="/images/logo.png" width="200"> -->
         <logo-component></logo-component>
         <p class="display-3 font-weight-bold">VACO</p>
-        <p class="body-2 red--text text--darken-3">A free webapp to manage tasks for virtually anything</p>
-          <v-btn color="indigo" to="signup">Sign up</v-btn>
-          <v-btn color="indigo" to="dashboard">Dashboard</v-btn>
+        <p class="body-2 red--text text--darken-3">You are offline and cannot view this page</p>
+          <v-btn color="indigo" to="/">Home</v-btn>
       </v-col>
     </v-row>
   </div>
@@ -18,36 +17,7 @@
 import AppBarComponent from '@/components/AppBarComponent'
 import LogoComponent from '@/components/LogoComponent'
 
-const check = () => {
-  if (!('serviceWorker' in navigator)) {
-    throw new Error('No Service Worker support!')
-  }
-  if (!('PushManager' in window)) {
-    throw new Error('No Push API Support!')
-  }
-}
 
-const registerServiceWorker = async () => {
-  const registration = await navigator.serviceWorker.register('/firebase-messaging-sw.js')
-  return registration
-}
-
-const main = async () => {
-  try{
-    check();
-    let registration = await registerServiceWorker()
-    if(registration){
-      console.log('SW is registered')
-     }
-  }
-  catch(error){
-    console.log('Service Worker ', error.message)
-  }
-}
-
-if (process.browser) {
-  main()
-}
 
 export default {
   middleware : [],
